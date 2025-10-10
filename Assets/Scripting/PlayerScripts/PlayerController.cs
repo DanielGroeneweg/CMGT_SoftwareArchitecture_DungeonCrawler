d0fcdd51303d06c43f1f0f1326bcc428456acb99
Daniël Groeneweg <daniel.groeneweg@ziggo.nl>
2025-10-10 14:56:09 +0200

    kermit suicide

--- Assets/Scripting/PlayerScripts/PlayerController.cs
+++ Assets/Scripting/PlayerScripts/PlayerController.cs
@@ -1,100 +1,167 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using System;
-using UnityEngine.EventSystems;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
+using UnityEngine.InputSystem;
+using System.Reflection;
+
 namespace CMGTSA
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerController : MonoBehaviour
     {
         #region Variables
         #region Inspector
-        [Serializable]
-        private class InputControl
-        {
-            public ActionTypes action;
-            public InputTypes input;
-            [ShowIf("input", ComparisonTypes.Equals, InputTypes.Keyboard)]
-            public KeyCode keyCode;
-            [ShowIf("input", ComparisonTypes.Equals, InputTypes.Mouse)]
-            public MouseButton button;
-        }
         [Header("Stats")]
-        [SerializeField] private float moveSpeed = 10;
-        [SerializeField] private float maxMoveSpeed = 10;
-        [SerializeField] private float groundedMaxRange = 0.2f;
-        [Tooltip("Top control takes top priority in actions the player takes")]
-        [SerializeField] private List<InputControl> controls = new List<InputControl>();
+        [SerializeField] private float groundedRange;
+        [SerializeField] private float jumpForce;
+        [SerializeField] private float attackDuration;
         [Header("References")]
+        [SerializeField] private SpriteRenderer sprite;
         [SerializeField] private Animator animator;
         [SerializeField] private Rigidbody2D rb;
-        [SerializeField] private SpriteRenderer sprite;
         #endregion
 
-        #region Internal
-        private bool grounded = false;
-        private bool jumping = false;
-        private bool attacking = false;
-        private Directions moveDirection;
+        #region Public
+
+        #endregion
+
+        #region Private
+        Vector2 move;
+        bool pressedJump = false;
+        bool pressedAttack = false;
+        bool isAttacking = false;
+        bool isJumping = false;
+        bool grounded = false;
+        bool isRunning = false;
+        bool isIdling = false;
         #endregion
         #endregion
-        private void Update()
-        {
-            grounded = CheckGrounded();
-
-            foreach (InputControl control in controls)
-            {
-                bool input = false;
-
-                if (control.input == InputTypes.Keyboard && Input.GetKey(control.keyCode)) input = true;
-                else if (control.input == InputTypes.Mouse && Input.GetMouseButtonDown(0)) input = true;
-
-                if (input)
-                {
-                    switch (control.action)
-                    {
-                        case ActionTypes.Attack:
-                            if (!attacking && !jumping) attacking = true;
-                            break;
-                        case ActionTypes.Jump:
-                            if (grounded && !attacking) jumping = true;
-                            break;
-                        case ActionTypes.MoveLeft:
-                            if (grounded && !attacking) moveDirection = Directions.Left;
-                            break;
-                        case ActionTypes.MoveRight:
-                            if (grounded && !attacking) moveDirection = Directions.Right;
-                            break;
-                    }
-                }
-            }
-        }
         private void FixedUpdate()
         {
+            CheckGrounded();
 
+            Jumping();
+
+            Attacking();
+
+            Movement();
+
+            ResetInput();
         }
-        private bool CheckGrounded()
+        private void Jumping()
         {
-            foreach (RaycastHit hit in Physics.RaycastAll(transform.position, Vector3.down, groundedMaxRange))
+            // Reset Jumping
+            if (grounded && isJumping)
             {
-                if (hit.transform.tag == "Ground") return true;
+                isJumping = false;
             }
 
-            return false;
-        }
-        #region additional
-        private void OnValidate()
-        {
-            foreach (ActionTypes actionType in Enum.GetValues(typeof(ActionTypes)))
+            // Start Jumping
+            if (grounded && pressedJump && !isAttacking)
             {
-                // Example: Ensure each InputType has an entry in your controls list
-                if (!controls.Exists(c => c.action == actionType))
+                isIdling = false;
+                isRunning = false;
+                isJumping = true;
+                rb.AddForceY(jumpForce, ForceMode2D.Force);
+            }
+        }
+        private void Attacking()
+        {
+            if (pressedAttack && !isAttacking)
+            {
+                isRunning = false;
+                isIdling = false;
+                isAttacking = true;
+                SetAnimation(Animations.Attack);
+                StartCoroutine(DisableBool(nameof(isAttacking), false, attackDuration));
+            }
+        }
+        private void Movement()
+        {
+            if (!grounded || isAttacking || isJumping) return;
+
+            bool isMoving = move != Vector2.zero;
+
+            if (isMoving)
+            {
+                if (!isRunning)
                 {
-                    controls.Add(new InputControl { action = actionType, input = InputTypes.Keyboard });
+                    isRunning = true;
+                    isIdling = false;
+                    SetAnimation(Animations.Running);
+                }
+
+                sprite.flipX = move.x < 0;
+            }
+            else
+            {
+                if (!isIdling)
+                {
+                    isRunning = false;
+                    isIdling = true;
+                    StartCoroutine(DelayedIdle());
                 }
             }
+        }
+        private void CheckGrounded()
+        {
+            foreach (RaycastHit2D hit in Physics2D.RaycastAll(transform.position, Vector2.down, groundedRange))
+            {
+                if (hit.transform.tag == "Ground")
+                {
+                    isJumping = false;
+                    grounded = true;
+                    return;
+                }
+            }
+            grounded = false;
+        }
+        #region UtilityMethods
+        private void SetAnimation(Animations animation)
+        {
+            animator.SetTrigger(animation.ToString());
+        }
+        private IEnumerator DisableBool(string boolName, bool value, float time)
+        {
+            yield return new WaitForSeconds(time);
+
+            // Find a field with the given name (public or private)
+            FieldInfo field = GetType().GetField(boolName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            if (field != null && field.FieldType == typeof(bool))
+            {
+                field.SetValue(this, value);
+                Debug.Log("value set!");
+            }
+            else
+            {
+                Debug.LogWarning($"No bool field named '{boolName}' found on {name}.");
+            }
+        }
+        private IEnumerator DelayedIdle()
+        {
+            yield return new WaitForSeconds(0.1f);
+            if (isIdling) SetAnimation(Animations.Idling);
+        }
+        #endregion
+
+        #region Input
+        public void OnMove(InputValue _Input)
+        {
+            move = _Input.Get<Vector2>();
+        }
+        public void OnJump(InputValue _Input)
+        {
+            pressedJump = true;
+        }
+        public void OnAttack(InputValue _Input)
+        {
+            pressedAttack = true;
+        }
+        private void ResetInput()
+        {
+            pressedJump = false;
+            pressedAttack = false;
         }
         #endregion
     }
--- Assets/Scripting/Utils/Enums.cs
+++ Assets/Scripting/Utils/Enums.cs
@@ -2,5 +2,6 @@ namespace CMGTSA
 {
     public enum ActionTypes { Attack, Jump, MoveLeft, MoveRight }
     public enum InputTypes { Mouse, Keyboard }
-    public enum Directions { Left, Right, Up, Down }
+    public enum Directions { Left, Right, Up, Down, None }
+    public enum Animations { Jumping, Idling, Falling, Attack, Dying, Running }
 }
